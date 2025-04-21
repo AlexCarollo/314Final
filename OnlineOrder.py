ce3f2c3a236fc009664e1150aeceb5c4a0686232
AlexCarollo <144820334+AlexCarollo@users.noreply.github.com>
2025-04-21 22:06:14 +0000

    Testing database connectivity

--- OnlineOrder.py
+++ OnlineOrder.py
@@ -1,17 +1,44 @@
 import mysql.connector
 from mysql.connector import errorcode
 
-def get_connection() -> mysql.connector.connection:
-    #Try to open the SQL connection:
-    try:
-        cnx = mysql.connector.connect(user='aajm', password='final314', host='cs314.iwu.edu', database='aajm')
-    
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            raise mysql.connector.Error("Something is wrong with your user name or password")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            raise mysql.connector.Error("Database does not exist")
-        else:
-            raise mysql.connector.Error(err)
+def main():
 
-    return cnx 
+    # Try to open the SQL connection...
+    try:
+
+        # This is how "modern" Python wants programmers to do "finally: close"
+        with mysql.connector.connect(user='aajm', password='final314', host='cs314.iwu.edu', database='aajm') as cnx:
+
+            # Same, but now with a cursor for your SQL work
+            try:
+                with cnx.cursor() as cursor:
+                    cursor.execute('SELECT * FROM Bmart')
+
+            # This code should handle any issues DURING SQL work.
+            except mysql.connector.Error as err:
+                print('Error while executing', cursor.statement, '--', str(err))
+
+                # If some part of our SQL queries errored, explicitly rollback all of them
+                # to "reset" the database back to its original state. Only needed for SQL
+                # queries that alter the database.
+                cnx.rollback()
+
+        # Any additional Python work that doesn't require the database, but does need the database data, can be
+        # performed here.
+        
+    # Connection errors handled here, with explicit handling for different types of errors.
+    except mysql.connector.Error as err:
+
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+            
+    #print(cnx is None or not cnx.is_connected())
+
+    print("In theory, the program could do other things here, including loop back and ask the user to try again.")
+
+if __name__ == "__main__":
+    main()
